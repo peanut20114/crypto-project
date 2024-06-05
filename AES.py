75e6e9c34b64e41ad41377e63c0ae310426a05cd
peanut <129077552+peanut20114@users.noreply.github.com>
2024-06-05 15:10:35 +0700

    Update AES.py

--- AES.py
+++ AES.py
@@ -1,8 +1,18 @@
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import padding
 import os
+from os import system, path
+import argparse
 
+try: 
+    import cryptography
+    from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+    from cryptography.hazmat.backends import default_backend
+    from cryptography.hazmat.primitives import padding
+except: 
+    system("pip install cryptography")
+    import cryptography
+    from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+    from cryptography.hazmat.backends import default_backend
+    from cryptography.hazmat.primitives import padding
 
 class AES:
     @staticmethod
@@ -29,10 +39,47 @@ class AES:
     def read_file_into_bytes(file_path):
         with open(file_path, 'rb') as file:
             file_bytes = file.read()
-        return file_bytes[:len(file_bytes)//2]
+        return file_bytes
 
     # Write bytes to file
     def write_bytes_to_file(file_path, data):
         with open(file_path, 'wb') as file:
             file.write(data)
+            
+            
+parser = argparse.ArgumentParser(description='Enter the path of the file to encrypt')
+parser.add_argument('choice', help='Encrypt or Decrypt')
+parser.add_argument('input_file', help='Path to the input MP4 file')
+file_name = parser.parse_args().input_file          
+choice = parser.parse_args().choice
+
+script_dir = path.dirname(__file__) #<-- absolute dir the script is in
+rel_path = "/video/" 
+
+save_path = path.join(script_dir, rel_path)
+print("script path:", script_dir)
+print('abs_file:', save_path)
+
+key = b"12345678abcdefgh"
+with open('iv.txt', 'rb') as file:
+    iv = file.read()   # iv = os.urandom(16) 16-bytes IV
         
+def encryptVideo(file_name):
+    aes_bytes = AES.read_file_into_bytes(file_name)
+    encrypted_aes_bytes = AES.encrypt(key, iv, aes_bytes)
+    with open('video/ciphertext.txt' , 'wb') as file: 
+        file.write(encrypted_aes_bytes)
+    
+def decryptVideo(file_name):
+    encrypted_aes_bytes = AES.read_file_into_bytes(file_name)
+    recovered = AES.decrypt(key, iv, encrypted_aes_bytes)
+    with open('video/recovered.mp4' , 'wb') as file: 
+        file.write(recovered)
+
+if choice == "encrypt":
+    encryptVideo(file_name)
+elif choice == "decrypt":
+    decryptVideo(file_name)
+else: 
+    print("Not supported!!!!") 
+    exit(1)
