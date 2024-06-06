656c24576fda982b6f915b8705909fd4bd8ead9b
peanut <129077552+peanut20114@users.noreply.github.com>
2024-06-06 13:10:09 +0700

    Update AES.py

--- AES.py
+++ AES.py
@@ -1,37 +1,52 @@
 import os
 from os import system, path
 import argparse
-
+import random 
 try: 
     import cryptography
     from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
     from cryptography.hazmat.backends import default_backend
-    from cryptography.hazmat.primitives import padding
+    from cryptography.hazmat.primitives import padding, hashes, serialization
+    from cryptography.hazmat.primitives.asymmetric import ec
+    from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+    from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+    import base64 
+
 except: 
     system("pip install cryptography")
     import cryptography
     from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
     from cryptography.hazmat.backends import default_backend
-    from cryptography.hazmat.primitives import padding
-
+    from cryptography.hazmat.primitives import padding, hashes, serialization
+    from cryptography.hazmat.primitives.asymmetric import ec
+    from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+    from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+    import base64 
+    
 class AES:
     @staticmethod
     def encrypt(key, iv, plaintext):
+        # Create a padder instance
+        padder = padding.PKCS7(algorithms.AES.block_size).padder()
+        # Add padding to the plaintext
+        padded_plaintext = padder.update(plaintext) + padder.finalize()
+        # Encrypt the padded plaintext
         backend = default_backend()
         cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
         encryptor = cipher.encryptor()
-        padder = padding.PKCS7(algorithms.AES.block_size).padder()
-        padded_plaintext = padder.update(plaintext) + padder.finalize()
         ciphertext = encryptor.update(padded_plaintext) + encryptor.finalize()
         return ciphertext
 
     @staticmethod
     def decrypt(key, iv, ciphertext):
+        # Decrypt the ciphertext
         backend = default_backend()
         cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
         decryptor = cipher.decryptor()
         decrypted_data = decryptor.update(ciphertext) + decryptor.finalize()
+        # Create an unpadder instance
         unpadder = padding.PKCS7(algorithms.AES.block_size).unpadder()
+        # Remove padding from the decrypted data
         plaintext = unpadder.update(decrypted_data) + unpadder.finalize()
         return plaintext
 
@@ -54,26 +69,108 @@ file_name = parser.parse_args().input_file
 choice = parser.parse_args().choice
 
 script_dir = path.dirname(__file__) #<-- absolute dir the script is in
-rel_path = "/video/" 
+rel_path = "/temp/" 
 
 save_path = path.join(script_dir, rel_path)
 print("script path:", script_dir)
 print('abs_file:', save_path)
 
-key = b"12345678abcdefgh"
-with open('iv.txt', 'rb') as file:
-    iv = file.read()   # iv = os.urandom(16) 16-bytes IV
+def generateRandomKeyandIV():
+    key = os.urandom(16)
+    iv = os.urandom(16)
+    with open('temp/AESkey.txt', 'wb') as file: 
+        file.write(key)
+    with open('temp/AESiv.txt', 'wb') as file: 
+        file.write(iv) 
+    return key, iv
+
+def encryptAESkey(key, private_key, public_key):
+    private_key = serialization.load_der_private_key(private_key, None)
+    public_key = serialization.load_der_public_key(public_key)
+    shared_key = private_key.exchange(ec.ECDH(), public_key)
+
+    # Perform key derivation.
+    derived_key = HKDF(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=None,
+        info=None,
+    ).derive(shared_key)
+
+    # Encrypt the AES key with the derived key
+    iv = os.urandom(16) # this iv is used for aes key encryption 
+    with open('temp/iv.txt', 'wb') as file: 
+        file.write(iv)
+    cipher = Cipher(algorithms.AES(derived_key), modes.CBC(iv))
+    encryptor = cipher.encryptor()
+    padder = padding.PKCS7(128).padder()
+    padded_data = padder.update(key) + padder.finalize()
+    encrypted_aes_key = encryptor.update(padded_data) + encryptor.finalize()
+    
+    return encrypted_aes_key
+
+def decryptAESkey(encrypted_aes_key, private_key, public_key):
+    private_key = serialization.load_der_private_key(private_key, None)
+    public_key = serialization.load_der_public_key(public_key)
+    shared_key = private_key.exchange(ec.ECDH(), public_key)
+
+    # Perform key derivation.
+    derived_key = HKDF(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=None,
+        info=None,
+    ).derive(shared_key)
+
+    # Decrypt the AES key with the derived key
+    with open('temp/iv.txt', 'rb') as file: 
+        iv = file.read()
+    cipher = Cipher(algorithms.AES(derived_key), modes.CBC(iv))
+    decryptor = cipher.decryptor()
+    unpadder = padding.PKCS7(128).unpadder()
+    decrypted_data = decryptor.update(encrypted_aes_key) + decryptor.finalize()
+    key = unpadder.update(decrypted_data) + unpadder.finalize()
+    
+    return key
         
-def encryptVideo(file_name):
+def encryptVideo(file_name, private_key, public_key):
+    key, iv = generateRandomKeyandIV()
     aes_bytes = AES.read_file_into_bytes(file_name)
     encrypted_aes_bytes = AES.encrypt(key, iv, aes_bytes)
-    with open('video/ciphertext.txt' , 'wb') as file: 
-        file.write(encrypted_aes_bytes)
     
-def decryptVideo(file_name):
+    # # Your base64 strings
+    # private_key_base64 = "MHcCAQEEIG1tm0lTR0XNgKsu33uyeP4ScxLkJ3ImTvnf3o47riZSoAoGCCqGSM49AwEHoUQDQgAES2cnfMIGauK4mqhTcc0dMXEjLAmcAy9tgJ5wXdCVwfRCrZrk4NdkBDOn2anwvxTMo4nzph8JtVM+ORMWf39ITQ=="
+    # public_key_base64 = "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAES2cnfMIGauK4mqhTcc0dMXEjLAmcAy9tgJ5wXdCVwfRCrZrk4NdkBDOn2anwvxTMo4nzph8JtVM+ORMWf39ITQ=="
+
+    # Convert base64 strings to bytes
+    private_key = base64.b64decode(private_key)
+    public_key = base64.b64decode(public_key)
+
+    print("Private Key Bytes:", private_key)
+    print("Public Key Bytes:", public_key)
+    
+    with open('temp/ciphertext.txt' , 'wb') as file: 
+        file.write(encrypted_aes_bytes)
+    with open('temp/encryptedAESkey.txt', 'wb') as file: 
+        file.write(encryptAESkey(key, private_key, public_key))
+    
+def decryptVideo(file_name, private_key, public_key):
+    with open('temp/encryptedAESkey.txt', 'rb') as file: 
+        encrypted_aes_key = file.read()
+        
+    with open('temp/AESiv.txt', 'rb') as file: 
+        iv = file.read()  
+    #  # Your base64 strings
+    # private_key_base64 = "MHcCAQEEIG1tm0lTR0XNgKsu33uyeP4ScxLkJ3ImTvnf3o47riZSoAoGCCqGSM49AwEHoUQDQgAES2cnfMIGauK4mqhTcc0dMXEjLAmcAy9tgJ5wXdCVwfRCrZrk4NdkBDOn2anwvxTMo4nzph8JtVM+ORMWf39ITQ=="
+    # public_key_base64 = "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAES2cnfMIGauK4mqhTcc0dMXEjLAmcAy9tgJ5wXdCVwfRCrZrk4NdkBDOn2anwvxTMo4nzph8JtVM+ORMWf39ITQ=="
+
+    # Convert base64 strings to bytes
+    private_key = base64.b64decode(private_key)
+    public_key = base64.b64decode(public_key)
+    key = decryptAESkey(encrypted_aes_key, private_key, public_key)
     encrypted_aes_bytes = AES.read_file_into_bytes(file_name)
     recovered = AES.decrypt(key, iv, encrypted_aes_bytes)
-    with open('video/recovered.mp4' , 'wb') as file: 
+    with open('temp/recovered.mp4' , 'wb') as file: 
         file.write(recovered)
 
 if choice == "encrypt":
@@ -83,3 +180,6 @@ elif choice == "decrypt":
 else: 
     print("Not supported!!!!") 
     exit(1)
+    
+    
+
