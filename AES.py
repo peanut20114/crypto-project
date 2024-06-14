22f1c530fe8f4f6f4cf1be18a8931ab04cff01db
peanut <129077552+peanut20114@users.noreply.github.com>
2024-06-14 10:47:14 +0700

    Update AES.py GCM mode

--- AES.py
+++ AES.py
@@ -1,47 +1,33 @@
 import os
 from os import system, path
 import argparse
-import random 
-try: 
+import random
+try:
     import cryptography
-    from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-    from cryptography.hazmat.backends import default_backend
-    from cryptography.hazmat.primitives import padding, hashes, serialization
+    from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+    from cryptography.hazmat.primitives import hashes, serialization
     from cryptography.hazmat.primitives.asymmetric import ec
     from cryptography.hazmat.primitives.kdf.hkdf import HKDF
-    from cryptography.hazmat.primitives.ciphers.aead import AESGCM
-    import base64 
-
-except: 
+    import base64
+except:
     system("pip install cryptography")
-    import cryptography
-    from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-    from cryptography.hazmat.backends import default_backend
-    from cryptography.hazmat.primitives import padding, hashes, serialization
+    from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+    from cryptography.hazmat.primitives import hashes, serialization
     from cryptography.hazmat.primitives.asymmetric import ec
     from cryptography.hazmat.primitives.kdf.hkdf import HKDF
-    from cryptography.hazmat.primitives.ciphers.aead import AESGCM
     import base64
 
 class AES:
     @staticmethod
-    def encrypt(key, iv, plaintext):
-        padder = padding.PKCS7(algorithms.AES.block_size).padder()
-        padded_plaintext = padder.update(plaintext) + padder.finalize()
-        backend = default_backend()
-        cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
-        encryptor = cipher.encryptor()
-        ciphertext = encryptor.update(padded_plaintext) + encryptor.finalize()
+    def encrypt(key, nonce, plaintext):
+        aesgcm = AESGCM(key)
+        ciphertext = aesgcm.encrypt(nonce, plaintext, None)
         return ciphertext
 
     @staticmethod
-    def decrypt(key, iv, ciphertext):
-        backend = default_backend()
-        cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
-        decryptor = cipher.decryptor()
-        decrypted_data = decryptor.update(ciphertext) + decryptor.finalize()
-        unpadder = padding.PKCS7(algorithms.AES.block_size).unpadder()
-        plaintext = unpadder.update(decrypted_data) + unpadder.finalize()
+    def decrypt(key, nonce, ciphertext):
+        aesgcm = AESGCM(key)
+        plaintext = aesgcm.decrypt(nonce, ciphertext, None)
         return plaintext
 
     @staticmethod
@@ -54,27 +40,26 @@ class AES:
     def write_bytes_to_file(file_path, data):
         with open(file_path, 'wb') as file:
             file.write(data)
-            
-            
-def generateRandomKeyandIV(output_dir):
-    key = os.urandom(16)
-    iv = os.urandom(16)
+
+def generateRandomKeyAndNonce(output_dir):
+    key = AESGCM.generate_key(bit_length=128)
+    nonce = os.urandom(12)
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
-    with open(os.path.join(output_dir, 'AESkey.txt'), 'wb') as file: 
+    with open(os.path.join(output_dir, 'AESkey.txt'), 'wb') as file:
         file.write(key)
-    with open(os.path.join(output_dir, 'AESiv.txt'), 'wb') as file: 
-        file.write(iv) 
-    return key, iv
+    with open(os.path.join(output_dir, 'AESnonce.txt'), 'wb') as file:
+        file.write(nonce)
+    return key, nonce
 
 def encryptAESkey(key_path, sender_private_key_path, receiver_public_key_path, output_dir):
-    with open(key_path, 'rb') as file: 
+    with open(key_path, 'rb') as file:
         key = file.read()
     with open(sender_private_key_path, 'rb') as key_file:
         sender_private_key_pem = key_file.read()
     with open(receiver_public_key_path, 'rb') as key_file:
         receiver_public_key_pem = key_file.read()
-    
+
     sender_private_key = serialization.load_pem_private_key(sender_private_key_pem, password=None)
     receiver_public_key = serialization.load_pem_public_key(receiver_public_key_pem)
     shared_key = sender_private_key.exchange(ec.ECDH(), receiver_public_key)
@@ -86,25 +71,25 @@ def encryptAESkey(key_path, sender_private_key_path, receiver_public_key_path, o
         info=None,
     ).derive(shared_key)
 
-    iv = os.urandom(16)
-    with open(os.path.join(output_dir, 'iv.txt'), 'wb') as file: 
-        file.write(iv)
-    cipher = Cipher(algorithms.AES(derived_key), modes.CBC(iv))
-    encryptor = cipher.encryptor()
-    padder = padding.PKCS7(128).padder()
-    padded_data = padder.update(key) + padder.finalize()
-    encrypted_aes_key = encryptor.update(padded_data) + encryptor.finalize()
-    with open(os.path.join(output_dir, 'AESkey.txt'), 'wb') as file: 
+    nonce = os.urandom(12)
+    with open(os.path.join(output_dir, 'nonce.txt'), 'wb') as file:
+        file.write(nonce)
+    
+    aesgcm = AESGCM(derived_key)
+    encrypted_aes_key = aesgcm.encrypt(nonce, key, None)
+    
+    with open(os.path.join(output_dir, 'AESkey.txt'), 'wb') as file:
         file.write(encrypted_aes_key)
     return encrypted_aes_key
 
 def decryptAESkey(encrypted_aes_key_path, receiver_private_key_path, sender_public_key_path, output_dir):
-    with open(encrypted_aes_key_path, 'rb') as file: 
+    with open(encrypted_aes_key_path, 'rb') as file:
         encrypted_aes_key = file.read()
     with open(receiver_private_key_path, 'rb') as key_file:
         receiver_private_key_pem = key_file.read()
     with open(sender_public_key_path, 'rb') as key_file:
         sender_public_key_pem = key_file.read()
+    
     receiver_private_key = serialization.load_pem_private_key(receiver_private_key_pem, password=None)
     sender_public_key = serialization.load_pem_public_key(sender_public_key_pem)
     shared_key = receiver_private_key.exchange(ec.ECDH(), sender_public_key)
@@ -116,40 +101,39 @@ def decryptAESkey(encrypted_aes_key_path, receiver_private_key_path, sender_publ
         info=None,
     ).derive(shared_key)
 
-    with open(os.path.join(output_dir, 'iv.txt'), 'rb') as file: 
-        iv = file.read()
-    cipher = Cipher(algorithms.AES(derived_key), modes.CBC(iv))
-    decryptor = cipher.decryptor()
-    unpadder = padding.PKCS7(128).unpadder()
-    decrypted_data = decryptor.update(encrypted_aes_key) + decryptor.finalize()
-    key = unpadder.update(decrypted_data) + unpadder.finalize()
-    with open(encrypted_aes_key_path, 'wb') as file: 
+    with open(os.path.join(output_dir, 'nonce.txt'), 'rb') as file:
+        nonce = file.read()
+    
+    aesgcm = AESGCM(derived_key)
+    key = aesgcm.decrypt(nonce, encrypted_aes_key, None)
+    
+    with open(encrypted_aes_key_path, 'wb') as file:
         file.write(key)
     return key
 
 def encryptVideo(file_name):
     base_file_name = os.path.splitext(os.path.basename(file_name))[0]
     output_dir = f'D:/{base_file_name}_temp'
-    key, iv = generateRandomKeyandIV(output_dir)
+    key, nonce = generateRandomKeyAndNonce(output_dir)
     aes_bytes = AES.read_file_into_bytes(file_name)
-    encrypted_aes_bytes = AES.encrypt(key, iv, aes_bytes)
-    output_name = base_file_name+'.txt'
+    encrypted_aes_bytes = AES.encrypt(key, nonce, aes_bytes)
+    output_name = base_file_name + '.txt'
     encrypted_file_path = os.path.join(output_dir, output_name)
     AES.write_bytes_to_file(encrypted_file_path, encrypted_aes_bytes)
     return encrypted_file_path
 
-def decryptVideo(file_name, key_path, iv_path):
+def decryptVideo(file_name, key_path, nonce_path):
     base_file_name = os.path.splitext(os.path.basename(file_name))[0]
     output_dir = f'D:/{base_file_name}_temp'
-    
-    with open(key_path, 'rb') as file: 
-       key = file.read()
-        
-    with open(iv_path, 'rb') as file: 
-        iv = file.read()
+
+    with open(key_path, 'rb') as file:
+        key = file.read()
+
+    with open(nonce_path, 'rb') as file:
+        nonce = file.read()
 
     encrypted_aes_bytes = AES.read_file_into_bytes(file_name)
-    recovered = AES.decrypt(key, iv, encrypted_aes_bytes)
+    recovered = AES.decrypt(key, nonce, encrypted_aes_bytes)
     recovered_file_path = os.path.join(output_dir, 'recovered.mp4')
     AES.write_bytes_to_file(recovered_file_path, recovered)
     return recovered_file_path
@@ -161,7 +145,7 @@ if __name__ == "__main__":
     parser.add_argument('--public_key', help="Path to the receiver's public key (for encryption) or sender's public key (for decryption)", default=None)
     parser.add_argument('--private_key', help="Path to the sender's private key (for encryption) or receiver's private key (for decryption)", default=None)
     parser.add_argument('--key_path', default=None)
-    parser.add_argument('--iv_path', default=None)
+    parser.add_argument('--nonce_path', default=None)
     parser.add_argument('--output_dir', default=None)
     args = parser.parse_args()
 
@@ -169,25 +153,24 @@ if __name__ == "__main__":
     choice = args.choice
     public_key_path = args.public_key
     private_key_path = args.private_key
-    key_path = args.key_path 
-    iv_path = args.iv_path
+    key_path = args.key_path
+    nonce_path = args.nonce_path
     output_dir = args.output_dir
-    
+
     if choice.lower() == "encrypt":
         encryptVideo(file_name)
     elif choice.lower() == "decrypt":
-        if not key_path or not iv_path:
-            print("key_path and iv_path are required for decrypting a video.")
+        if not key_path or not nonce_path:
+            print("key_path and nonce_path are required for decrypting a video.")
             exit(1)
-        decryptVideo(file_name, key_path, iv_path)
+        decryptVideo(file_name, key_path, nonce_path)
     elif choice.lower() == "encryptaeskey":
-        if not file_name and not private_key_path and not public_key_path:
+        if not file_name or not private_key_path or not public_key_path:
             print("Sender private key and receiver public key are required to encrypt AES key")
             exit(1)
         encryptAESkey(file_name, private_key_path, public_key_path, output_dir)
     elif choice.lower() == "decryptaeskey":
         decryptAESkey(file_name, private_key_path, public_key_path, output_dir)
-        
     else:
         print("Invalid choice. Supported choices are: encrypt, decrypt, encryptAESkey, decryptAESkey.")
         exit(1)
